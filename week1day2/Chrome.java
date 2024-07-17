5bb53c2918e71ad737a2c70832376b55a8e29c63
poojadurai-86 <poojamalur12@gmail.com>
2024-07-17 09:21:18 +0530

    Add files via upload

---
+++ week1day2/Chrome.java
@@ -0,0 +1,16 @@
+package week1day2;
+
+public class Chrome {
+	
+	public static void main(String[] args) {
+		float versionType =91.0f;
+		String developerType ="Google";
+		boolean isBeta = false;
+		int releaseYear=2008;
+		char shortcutKey ='c';
+		
+	System.out.println(versionType+"\n" + developerType+"\n" +isBeta+"\n"+releaseYear+"\n"+releaseYear+ "\n"+shortcutKey+"\n");
+		}
+	}
+
+
---
+++ week1day2/FireFox.java
@@ -0,0 +1,14 @@
+package week1day2;
+
+public class FireFox {
+	
+	public static void main(String args[]) {
+	float browserVersion=100.2f;
+	String browserName="firefox";
+	boolean isVisible=true;
+	int releaseYear=1998;
+	char browserLogo='f';
+	
+System.out.println(browserVersion+"\n" +browserName+"\n" +isVisible+"\n"+releaseYear+"\n"+browserLogo+"\n");
+	}
+}
